1ede799545fd736ba8d9be3c69a4bae26ee7f973
Mingyu Liu <whoopsandy@gmail.com>
2014-10-25 00:00:00 -0400

    additional comments

--- isInterleave.java
+++ isInterleave.java
@@ -1,9 +1,17 @@
 // naive approach got TLEed (O(N^2)? NEED TO BE VERIFIED)
 // used hashmap to store visited cases
 
+// second thought: since it only continues searching if it has not found a solution yet. hashmap could change to set to save some sapce
+
+// third though: if recursive function takes two parameters, we could use 2 dimension array for memoization
+
+/* TODO
+	Use matrix to store info: change to interative solution
+*/
 public class Solution {
     HashMap<String, Boolean> map = new HashMap<String, Boolean>();
     public boolean isInterleave(String s1, String s2, String s3) {
+    	// could change map to set, only contain keys
         if (map.containsKey(s1+ "+" + s2)) return map.get(s1 + "+" + s2);
         if (s1.length() + s2.length() != s3.length()) return false;
         if (s1.length() == 0 || s2.length() == 0) {
