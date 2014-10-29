ae1d61d577afec04bdd900bfebbc38d13d84f514
Mingyu Liu <whoopsandy@gmail.com>
2014-10-29 18:52:14 -0400

    accepted exist solution

--- exist.java
+++ exist.java
@@ -1,7 +1,6 @@
-// got TLEed
+// be very carefull with base case !!!!
+
 class Solution {
-     Map<String, Boolean> set = new HashMap<String, Boolean>();
-    Set<Integer> tracking = new HashSet<Integer>();
 
     public boolean exist(char[][] board, String word) {
         if (board == null || board.length == 0
@@ -11,13 +10,11 @@ class Solution {
 
         int m = board.length;
         int n = board[0].length;
-        
-        if (word.length() > m * n) return false;
 
+        if (word.length() > m * n) return false;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
-               if(check(board, i, j, word)) return true;
+                    if(check(board, i, j, word, new HashSet<Integer>())) return true;
 
             }
         }
@@ -25,37 +22,38 @@ class Solution {
         return false;
     }
 
-    public boolean check(char[][] board, int row, int col, String word) {
-        if (word.length() == 0) return true;
-        if(set.containsKey(row + " " + col + " " + word.length())) return set.get(row + " " + col + " " + word.length());
+    public boolean check(char[][] board, int row, int col, String word, HashSet<Integer> tracking) {
+      
+        if (word.length() == 1 && word.charAt(0) == board[row][col]) return true;
+
 
         int m = board.length;
         int n = board[0].length;
 
         boolean success = false;
-        
+
         if (word.charAt(0) == board[row][col] && word.length() <= m * n - tracking.size()) {
             tracking.add(row * n + col);
             if (row + 1 < m && !tracking.contains((row + 1) * n + col)) {
-                success = check(board, row + 1, col, word.substring(1));
+                success = check(board, row + 1, col, word.substring(1), tracking);
             }
             if (!success && row - 1 >= 0 && !tracking.contains((row - 1) * n + col)) {
 
-                success = check(board, row - 1, col, word.substring(1));
+                success = check(board, row - 1, col, word.substring(1), tracking);
             }
             if (!success && col + 1 < n && !tracking.contains(row * n + col + 1)) {
 
-                success = check(board, row, col + 1, word.substring(1));
+                success = check(board, row, col + 1, word.substring(1), tracking);
             }
             if (!success && col - 1 >= 0 && !tracking.contains(row * n + col - 1)) {
 
-                success = check(board, row, col - 1, word.substring(1));
+                success = check(board, row, col - 1, word.substring(1), tracking);
             }
 
             tracking.remove(Integer.valueOf(row * n + col));
         }
 
-        set.put(row + " " + col + " " + word.length(), success);
+
         return success;
     }
 }
