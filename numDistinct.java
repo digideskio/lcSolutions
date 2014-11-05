84e55997c0f5e2fe71b2ceb28fa7fa5d6492d35e
Mingyu Liu <whoopsandy@gmail.com>
2014-11-05 12:08:48 -0500

    added optimal solution

--- numDistinct.java
+++ numDistinct.java
@@ -19,5 +19,36 @@ public class Solution {
         
         return table[T.length()][S.length()];
     }
+
+    // optimal solution that uses only O(n) space
+
+    public int numDistinct(String S, String T) {
+        if (S.length() < T.length()) return 0;
+        
+        int[] table = new int[S.length()+1];
+        for (int i = 0; i <= S.length(); i++) {
+            table[i] = 1;
+        }
+        int last = 0;
+        int cur = 0;
+        for (int i = 1; i <= T.length(); i++) {
+            last = 0;
+            for (int j = 1; j <= S.length(); j++) {
+                if (S.charAt(j-1) == T.charAt(i-1)) {
+                    cur = last + table[j-1];
+                    //table[i][j] = table[i-1][j-1] + table[i][j-1];
+                } else {
+                    //table[i][j] = table[i][j-1];
+                    cur = last;
+                }
+                
+                table[j-1] = last;
+                last = cur;
+            }
+            table[S.length()] = cur;
+        }
+        
+        return table[S.length()];
+    }
     
 }
