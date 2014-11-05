bf462ef9b329fd2e334e04d96545413d167b6972
Mingyu Liu <whoopsandy@gmail.com>
2014-11-05 11:56:45 -0500

    added num of distinct Subsequences

---
+++ numDistinct.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int numDistinct(String S, String T) {
+        if (S.length() < T.length()) return 0;
+        
+        int[][] table = new int[T.length()+1][S.length()+1];
+        for (int i = 0; i <= S.length(); i++) {
+            table[0][i] = 1;
+        }
+        
+        for (int i = 1; i <= T.length(); i++) {
+            for (int j = 1; j <= S.length(); j++) {
+                if (S.charAt(j-1) == T.charAt(i-1)) {
+                    table[i][j] = table[i-1][j-1] + table[i][j-1];
+                } else {
+                    table[i][j] = table[i][j-1];
+                }
+            }
+        }
+        
+        return table[T.length()][S.length()];
+    }
+    
+}
